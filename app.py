7aba59d85029cd7d338f49676c7a814f7f66271b
Puja Ghodake <misssonar27@gmail.com>
2026-07-31 07:25:07 +0530

    added Vote, Result and Reset functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,29 @@ def home():
 @app.route("/health")
 def health():
     return "App is running"
+
+# Dictionary to store votes
+votes = {}
+
+# Vote for a candidate
+@app.route("/vote/<name>")
+def vote(name):
+    if name in votes:
+        votes[name] += 1
+    else:
+        votes[name] = 1
+
+    return f"Vote recorded for {name}. Total votes: {votes[name]}"
+
+@app.route("/reset")
+def reset():
+    votes.clear()  # Remove all votes
+    return "All vote counts have been reset."
+
+# Show results
+@app.route("/results")
+def results():
+    return jsonify(votes)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
